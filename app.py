e19b1155d131f5ce90a6adafd33e40116d16c91b
Pratisha <varshney.pratisha@gmail.com>
2020-04-10 12:27:53 +0530

    added for heroku

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from resources.store import Stores, StoreList
 from db import db
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/flaskRest'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/flaskRest'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'pratisha'
 # app.config['JWT_AUTH_URL_RULE'] = '/login'
